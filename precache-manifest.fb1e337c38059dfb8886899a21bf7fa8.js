6d4feb3c2fe419307245b0db8f1104d27f4f9dda
Lucien <lygm1062@gmail.com>
2019-01-19 15:44:51 +0800

    normal update

---
+++ precache-manifest.fb1e337c38059dfb8886899a21bf7fa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4b87cefe51d3c9fdebd",
+    "url": "/bull/static/css/main.cdfa654b.chunk.css"
+  },
+  {
+    "revision": "b4b87cefe51d3c9fdebd",
+    "url": "/bull/static/js/main.b4b87cef.chunk.js"
+  },
+  {
+    "revision": "e7d31d35c8f7c7614411",
+    "url": "/bull/static/css/1.310f2b25.chunk.css"
+  },
+  {
+    "revision": "e7d31d35c8f7c7614411",
+    "url": "/bull/static/js/1.e7d31d35.chunk.js"
+  },
+  {
+    "revision": "2f422e2bdd47456142b1",
+    "url": "/bull/static/js/runtime~main.2f422e2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bull/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9d572cd5e5d1f8755a87e1cc528aa9c",
+    "url": "/bull/static/media/info.c9d572cd.png"
+  },
+  {
+    "revision": "dbe5f0b562ed04679a01265a1b044bdc",
+    "url": "/bull/index.html"
+  }
+];
